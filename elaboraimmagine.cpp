9eb091539a2572c3d9b33e0299eab278cad654fd
Fabiano <fabianotavallini@gmail.com>
2016-07-28 15:27:02 +0000

    Primi passi

--- elaboraimmagine.cpp
+++ elaboraimmagine.cpp
@@ -12,14 +12,24 @@ int main(int argc, char* argv[]) {
     
     char* fileinput = argv[1];
     char* comando = argv[2];
-    double dimensioni_x = argv[3];
-    double dimensioni_y = argv[4];
+    double dimensioni_x = atof(argv[3]);
+    double dimensioni_y = atof(argv[4]);
     char* fileoutput = argv[5];
+    
+    if (!fileinput || !comando || dimensioni_x < 0 || dimensioni_y < 0 || !fileoutput) {
+        cout << "missing params\n";
+        return 0;
+    }
     
     cout    << "fileinput: " << fileinput << "\n"
             << "comando: " << comando << "\n"
             << "dimensioni_x: " << dimensioni_x << "\n"
             << "dimensioni_y: " << dimensioni_y << "\n"
-            << "fileoutput: " << fileoutput;
+            << "fileoutput: " << fileoutput << "\n";
+            
+    
+    
+    
+    
     
 }
